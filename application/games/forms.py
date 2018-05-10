8919990b0c24d5f34ac8889063b83b7c9ea009c7
Ollikehy <eragah@gmail.com>
2018-05-10 12:29:15 +0300

    lisätty tyylejä ja validointia

--- application/games/forms.py
+++ application/games/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SelectField, validators
+from wtforms import StringField, SelectField, IntegerField, validators
 
 from application import db
 from sqlalchemy.sql import text
@@ -7,7 +7,7 @@ from application.developer.models import Developer
 
 class GameForm(FlaskForm):
     name = StringField("Name", [validators.Length(min=1)])
-    releaseyear = StringField("Release year", [validators.Length(min=4)])
+    releaseyear = IntegerField("Release year", [validators.Length(min=4)])
     genre = SelectField("Genre", choices=[('Action','Action'),('Adventure','Adventure'),
     ('Fighter','Fighter'),('Indie','Indie'),('Music','Music'),('Other','Other'),('Puzzle','Puzzle'),
     ('RPG','RPG'),('Shooter','Shooter'),('Simulation','Simulation'),('Sports','Sports')])
